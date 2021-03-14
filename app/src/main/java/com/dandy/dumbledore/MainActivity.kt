5af4d6caa22c5e4eca1d01484b4b243bbabdcaef
flycatdeng <dengchukun@qq.com>
2021-03-14 19:00:05 +0800

    初始提交，kotlin运行

---
+++ app/src/main/java/com/dandy/dumbledore/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dandy.dumbledore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
